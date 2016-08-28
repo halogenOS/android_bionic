60f1e029ffd19facf5501c0c76aa72a23df667e5
Christopher N. Hesse <raymanfx@gmail.com>
2016-08-28 15:29:08 +0200

    linker: Allow text relocations
    Change-Id: I672b5df08ec8d7e623d83da1f402013e555eca2a
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -4017,8 +4017,12 @@ bool soinfo::link_image(const soinfo_list_t& global_group, const soinfo_list_t& 
 #if !defined(__i386__) // ffmpeg says that they require text relocations on x86
     if (get_application_target_sdk_version() > 22) {
       PRINT("%s: has text relocations", get_realpath());
+#if defined(ALLOW_TEXT_RELOCATIONS)
+      DL_WARN("%s: has text relocations", get_realpath());
+#else
       DL_ERR("%s: has text relocations", get_realpath());
       return false;
+#endif
     }
 #endif
     // Make segments writable to allow text relocations to work properly. We will later call
