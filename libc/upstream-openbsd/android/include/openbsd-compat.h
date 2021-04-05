a2756a2f15d38c912d227847766ebec0b5d624cd
Simão Gomes Viana <devel@superboring.dev>
2021-04-05 15:33:57 +0200

    libc: remove conflicting explicit_bzero in header
    Change-Id: I8acadb2b017ae2c19338fc40a74ade3981594949

--- libc/upstream-openbsd/android/include/openbsd-compat.h
+++ libc/upstream-openbsd/android/include/openbsd-compat.h
@@ -23,6 +23,8 @@
 
 #include <stddef.h> // For size_t.
 
+#include <string.h> // For explicit_bzero
+
 #include <sys/random.h> // For getentropy.
 
 #include "private/bsd_sys_param.h"
@@ -56,8 +58,6 @@ extern const char* __progname;
 
 /* OpenBSD has this, but we can't really implement it correctly on Linux. */
 #define issetugid() 0
-
-#define explicit_bzero(p, s) memset(p, 0, s)
 
 /* OpenBSD has this in paths.h. But this directory doesn't normally exist.
  * Even when it does exist, only the 'shell' user has permissions.
